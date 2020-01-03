afdab7b6e23ac1ba5d03641fd8634f2eaf24f236
Daniel Guadalupe Sánchez <dguadalupe@novasolutionsystems.com>
2020-01-03 11:07:41 -0600

    Import in spect app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BlueboxdlsModule } from '@bluebox_dls/angular';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
@@ -7,6 +9,10 @@ describe('AppComponent', () => {
             declarations: [
                 AppComponent
             ],
+            imports: [
+                RouterTestingModule,
+                BlueboxdlsModule
+            ]
         }).compileComponents();
     }));
 
